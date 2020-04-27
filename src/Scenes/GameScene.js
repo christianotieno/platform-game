a0478e07eb5605a732600835643901c850c65f12
Christian Otieno <oduorchristian@gmail.com>
2020-04-27 04:16:07 +0300

    Fix timeloop event

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -26,25 +26,28 @@ export default class GameScene extends Phaser.Scene {
     this.createCoins();
     this.createBombs();
 
+    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    this.gameOverText = this.add.text(230, 260, 'Game Over', { fontSize: '64px', fill: '#000' });
+    this.gameOverText.setOrigin(0, 0);
+    this.gameOverText.visible = false;
+
+
     this.timerText = this.add.text(0, 100, `Countdown: ${this.initialTime}`, { fill: '#fff' });
 
     const onEvent = () => {
-      this.initialTime -= 1;
-      this.timerText.setText(`Countdown: ${this.initialTime}`);
+      do {
+        this.initialTime -= 1;
+        this.timerText.setText(`Countdown: ${this.initialTime}`);
+      }
+      while (this.initialTime < 0);
       if (this.initialTime === 0) {
-        this.gameOver = true;
-        this.gameOverText.visible = true;
-        this.scene.start('Title');
+        this.gameOver = true; this.gameOverText.visible = true;
       }
     };
+
     this.timedEvent = this.time.addEvent({
       delay: 1000, callback: onEvent, callbackScope: this, loop: true,
     });
-
-    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-    this.gameOverText = this.add.text(400, 300, 'Game Over', { fontSize: '64px', fill: '#000' });
-    this.gameOverText.setOrigin(0, 0);
-    this.gameOverText.visible = false;
   }
 
 
